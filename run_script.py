17c52c7fc723255f86b20212e968413a447835a5
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-09-12 16:57:55 -0500

    working on bonus for function exercises

--- run_script.py
+++ run_script.py
@@ -1,5 +1,7 @@
-def cumsum(numbers):
-    for i in range(1, len(numbers)):
-        numbers[i] += sum(numbers[i-1:i])
-    return numbers
-print(cumsum([1,2,3,4]))
+def col_index(column_name):
+    alpha = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z']
+    index = 0
+    for i in range(1,len(column_name)+1):
+        index += (alpha.index(column_name[-i].lower())+1 )* (26 ** (i-1))
+        return index
+print(col_index('AA'))
