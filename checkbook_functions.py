5f4cb687985272bea7e8766dabbc99e9148b21c2
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-09-15 10:27:23 -0500

    new function to standardize date from time stamp and one to wrap string text at a specific width

--- checkbook_functions.py
+++ checkbook_functions.py
@@ -10,23 +10,64 @@ def check_balance():
     return balance
 
 
-def make_transaction(amount,category,timestamp):
+def make_transaction(amount,category,description,timestamp):
     '''
     make_transaction() takes in transaction amount, category and the time the transaction was maded and stores them in transactions.csv
     '''
     import csv
-    fields = [amount, category, timestamp]
+    fields = [amount, category, description, timestamp]
     with open(r'transactions.csv', 'a') as file:
         writer = csv.writer(file)
         writer.writerow(fields)
     pass
 
+def add_description():
+    '''
+    Prompts user to add description
+    
+    returns user input for description if user opts to add one
 
-# def make_withdrawl(amt):
-#     import csv
-#     fields = [-amt]
-#     with open(r'transactions.csv', 'a') as file:
-#         writer = csv.writer(file)
-#         writer.writerow(fields)
-#     pass
+    else returns 'n/a'
+    '''
+    prompt_input = input("Would You Like to Add a Description? (y/n) ")
+    while prompt_input.lower() not in ['y','n']:
+        prompt_input = input("Invalid Response, Please Enter 'y' for Yes or 'n' for No ")
+    
+    if prompt_input.lower() == 'y':
+        return input("Please Enter Description. \n")
+        
+    else:
+        print('~No Description~')
+        return 'n/a'
+        
+def print_table():
+    print ('*-------------------------------------------------------------------*')
+    print ('|Date          |Amount    |Category  |Description                   |')
+    print ('*-------------------------------------------------------------------*')
+    
+
+
+
+
+def date_from_timestamp(timestamp):
+    from datetime import date, datetime
+    ugly_date = datetime.fromtimestamp(timestamp)
+    pretty_date = str(ugly_date)[5:7] + '/' + str(ugly_date)[8:10] + '/' + str(ugly_date)[2:4]
+    #print(str(ugly_date)[11:16])
+    time = str(ugly_date)[11:16]
+    return pretty_date + ' ' + time
+
+def string_wrap_text(string, width):
+    word_list = string.split(' ')
+    return_string = ''
+    running_len = 0
+    for i in word_list:
+        if len(return_string + i) - running_len < width:
+            return_string += (i + ' ')
+        else:
+            return_string += ('\n' + i + ' ') 
+            running_len = len(return_string)-len('\n' + i + ' ')
+    return return_string
+
+
 
--- python_checkbook.py
+++ python_checkbook.py
@@ -1,10 +1,11 @@
-from checkbook_functions import make_transaction, check_balance
-from time import time
+import csv
+from checkbook_functions import make_transaction, check_balance, add_description
+from time import time, sleep
 #Print welcome message and show options for input selection
 print("~~~~ Welcome to your Terminal Checkbook! ~~~~\n")
 action = ''
 while action != '5':
-    print("\nWhat would you like to do? \n\n1) Check Balance\n2) Record a Debit (Make a Deposit\n3) Record a Credit (Make a Withdrawl)\n4) See History\n5) Exit Application")
+    print("\nWhat would you like to do? \n\n1) Check Balance\n2) Record a Debit (Make a Deposit\n3) Record a Credit (Make a Withdrawl)\n4) Search History\n5) Exit Application")
 
 
     action = input("\nYour Choice? ")
@@ -12,20 +13,24 @@ while action != '5':
         action = input("Sorry, Your Selection is Invalid:\n\nPlease enter one of the following (1, 2, 3, or 4) ")
 
     if action == '1':
-        print(f"\nYour balance is ${check_balance()}\n")
+        print(f"\nYour balance is ${check_balance()}")
+        input("press <ENTER> to continue")
 
-    elif action == '2':
-        deposite_amount = input("\nHow much is the debit? ")
-        deposite_category = input("what is this transaction for? ")
-        deposite_time = time()
-        make_transaction(deposite_amount,deposite_category,deposite_time)
 
-    elif action == '3':
-        withdrawl_amount = input("\nHow much is the credit? ")
-        withdrawl_category = input("what is this transaction for? ")
-        withdrawl_time = time()
-        make_transaction(-1 * int(withdrawl_amount), withdrawl_category,withdrawl_time)
+    elif action == '2' or action == '3':
+        if action == '2':    
+            amount = input("\nHow much is the debit? ")
+        else:
+            amount = input("\nHow much is the credit? ")
+            amount = int(amount) * -1
+        category = input("what is this transaction for? ")
+        description = add_description()
+        time = time()
+        make_transaction(amount, category, description, time)
 
+    elif action == '4':
+        input('Would you like to...\a\a1) See All Transactions\n2) Filter Search')
+        
     elif action == '5':
         print("\nThanks and have a great day!\n")
         break
