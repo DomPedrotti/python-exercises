8c23e77a68a198b422a801bff20a16d39436ad80
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-09-19 11:50:58 -0500

    more exercises on matplotlib line plots

--- 4.6.1_matplotlib_exercises.py
+++ 4.6.1_matplotlib_exercises.py
@@ -1,12 +1,48 @@
 import matplotlib.pyplot as plt  
+import math
 #Use matplotlib to plot the following equation:
     #y = x**2 - x + 2
 #Add an anotation for the point 0, 0, the origin.
+x = range(-5,6)
+y = [i**2 - i + 2 for i in x]
+txt = '$\leftarrow Origin$'
 
-x = [i**2 - i + 2 for i in range(-10,10)]
-txt = '<- Origin'
 
-plt.plot(x)
+plt.plot(x,y)
+plt.plot(0,0,'o')
 plt.text(0,0, txt)
+plt.yticks(rotation = 90)
 plt.show()
+
+# Create and label 4 separate charts for the following equations (choose a range for x that makes sense):
+# y = sqrt(x)
+x = range(0,11)
+y = [i ** .5 for i in x]
+
+plt.plot(x,y)
+plt.show()
+# y = x^3
+x = range(-5,6)
+y = [i ** 3 for i in x]
+
+plt.plot(x,y)
+plt.show()
+
+# y = tan(x) 
+x = [i* .01 for i in range(-400,401)]
+y = [math.tan(i) for i in x]
+
+plt.ylim(-10,10)
+plt.plot(x,y)
+plt.show()
+
+# y = 2^x
+x = [i* .1 for i in range(-50,50)]
+y = [2 ** i for i in x]
+
+plt.plot(x,y)
+plt.show()
+
+
+
 
--- run_script.py
+++ run_script.py
@@ -1,1 +1,10 @@
-import pymysql
+import matplotlib.pyplot as plt 
+import math
+
+
+x = [i* .1 for i in range(-50,50)]
+y = [2 ** i for i in x]
+
+
+plt.plot(x,y)
+plt.show()
