b8cd9a485dcb1e065b074b1eeb2b9a6bf9a55c75
Dominic Pedrotti <pedrotti.dom@gmail.com>
2019-09-23 10:22:08 -0500

    6/8 numpy exercises

--- 4.7_numpy_exercises.py
+++ 4.7_numpy_exercises.py
@@ -5,12 +5,15 @@ a = np.array([4, 10, 12, 23, -2, -1, 0, 0, 0, -6, 3, -7])
 print("There are {} negative numbers".format(len(a[a < 0])))
 # How many positive numbers are there?
 print("There are {} positive numbers".format(len(a[a > 0])))
-
 # How many even positive numbers are there?
-
+print("There are {} positive, even numbers".format(len(a[(a>0) & (a%2 == 0)])))
 # If you were to add 3 to each data point, how many positive numbers would there be?
-
+b = a + 3
+print("If we add 3, there are {} positive numbers".format(len(b[b > 0])))
 # If you squared each number, what would the new mean and standard deviation be?
+a_squared = a ** 2
+print("If we squared our array, our mean would change from {} to {}".format(np.mean(a), np.mean(a_squared)))
+print("If we squared our array, our mean would change from {} to {}".format(np.std(a), np.std(a_squared)))
 
 # A common statistical operation on a dataset is centering. This means to adjust the data such that the center of the data is at 0. This is done by subtracting the mean from each data point. Center the data set.
 
